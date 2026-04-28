40119e4803de3245e632c31ab5c738b9aaf64e68
22102 <@Rak1n>
2026-04-29 11:05:37 +1200

    adding max and min window resize also added back ground color

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from PySide6 import QtWidgets
-
+from PySide6 import QtWidgets, QtCore
+from PIL import Image
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        #self.resize(800,600)
+        self.setMinimumSize(QtCore.QSize(600,400))
+        self.setMaximumSize(QtCore.QSize(800,600))
+        self.setWindowTitle("MRGS Tuck Shop")
+        self.setWindowOpacity(1)
+        self.setStyleSheet("background-color: black")
+
 
 
 if __name__=="__main__":

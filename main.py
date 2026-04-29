1599a6e11a5b6214b6c14392f5edcd89a61d3edf
22102 <@Rak1n>
2026-04-30 09:37:25 +1200

    Added an window icons and added version and dsiplay name

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PySide6 import QtWidgets, QtCore
+from PySide6 import QtWidgets, QtCore, QtGui
 from PIL import Image
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -9,11 +11,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("MRGS Tuck Shop")
         self.setWindowOpacity(1)
         self.setStyleSheet("background-color: black")
-
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("Falcon.png"))
+        self.setWindowIcon(icon)
 
 
 if __name__=="__main__":
     app = QtWidgets.QApplication([])
+    app.setApplicationName("Riri's App")
+    app.setApplicationVersion("v1")
+    app.setApplicationDisplayName(f"{app.applicationName()} - {app.applicationVersion()}")
     ui = MainWindow()
     ui.show()
     app.exec()

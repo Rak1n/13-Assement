b860d1f19716702bb97d27e60d990e6daf35eb58
22102 <@Rak1n>
2026-05-01 14:59:48 +1200

    added an sys and text plus options to adjust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PySide6 import QtWidgets, QtCore, QtGui
 from PIL import Image
+import sys
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -10,18 +11,22 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setMaximumSize(QtCore.QSize(800,600))
         self.setWindowTitle("MRGS Tuck Shop")
         self.setWindowOpacity(1)
-        self.setStyleSheet("background-color: black")
+        self.setStyleSheet("background-color: #cc3628")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("Falcon.png"))
         self.setWindowIcon(icon)
+        self.label = QtWidgets.QLabel("MRGS Tuck", self)
+        self.label.setObjectName("label")
+        print(self.label.objectName())
+        self.label.setGeometry(200,100,300,200)
 
 
 if __name__=="__main__":
-    app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication(sys.argv)
     app.setApplicationName("Riri's App")
     app.setApplicationVersion("v1")
     app.setApplicationDisplayName(f"{app.applicationName()} - {app.applicationVersion()}")
     ui = MainWindow()
     ui.show()
-    app.exec()
+    sys.exit(app.exec())
 

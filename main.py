3bc5787656aec5cb5a3f3d81378c054161e9c5d3
22102 <@Rak1n>
2026-05-04 13:28:04 +1200

    added an secondary color base and adjusted label placement

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter.ttk import tclobjs_to_py
+
 from PySide6 import QtWidgets, QtCore, QtGui
 from PIL import Image
 import sys
@@ -18,9 +20,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.label = QtWidgets.QLabel("MRGS Tuck", self)
         self.label.setObjectName("label")
         print(self.label.objectName())
-        self.label.setGeometry(200,100,300,200)
-
-
+        self.label.setGeometry(QtCore.QRect(270,20,70,50)) #100 from the left ,200 from the top,width 300,height 200
+        self.label.setStyleSheet("background-color: blue;""color: black")
+        self.label2 = QtWidgets.QLabel("ko", self)
+        self.label2.setObjectName("l")
+        print(self.label2.objectName())
 if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
     app.setApplicationName("Riri's App")
